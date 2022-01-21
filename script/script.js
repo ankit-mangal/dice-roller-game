db95617dc916815355e23671833f78aaca442360
ankit-25m <mangalankit843@gmail.com>
2022-01-21 20:23:48 +0530

    dice roller game

---
+++ script/script.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('score--0').textContent = '0';
+  document.getElementById('score--1').textContent = '0';
+  document.getElementById('current--0').textContent = '0';
+  document.getElementById('current--1').textContent = '0';
+  document.getElementById('name--0').textContent = 'Player 1';
+  document.getElementById('name--1').textContent = 'Player 2';
+  document.querySelector('.player--0').classList.remove('player--winner');
+  document.querySelector('.player--1').classList.remove('player--winner');
+  document.querySelector('.player--0').classList.remove('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
+  document.querySelector('.player--0').classList.add('player--active');
+}
+
+init();
+// document.querySelector('#current--' + activePlayer).textContent = dice;
+
+document.querySelector('.btn--roll').addEventListener('click', () => {
+  if (gamePlaying) {
+    //1. Random Number generate
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display the dice and result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. Update round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+      roundScore += dice;
+      document.querySelector('#current--' + activePlayer).textContent =
+        roundScore;
+    } else {
+      nextPlayer();
+    }
+  }
+});
+
+document.querySelector('.btn--hold').addEventListener('click', function () {
+  //add current score to global score.
+  if (gamePlaying) {
+    scores[activePlayer] += roundScore;
+
+    //change the UI
+    document.querySelector('#score--' + activePlayer).textContent =
+      scores[activePlayer];
+
+    //check if player has won the game
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name--' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document
+        .querySelector('.player--' + activePlayer)
+        .classList.add('player--winner');
+      document
+        .querySelector('.player--' + activePlayer)
+        .classList.remove('player--active');
+      gamePlaying = false;
+    } else {
+      //new player
+      nextPlayer();
+    }
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById('current--0').textContent = '0';
+  document.getElementById('current--1').textContent = '0';
+  document.querySelector('.player--0').classList.toggle('player--active');
+  document.querySelector('.player--1').classList.toggle('player--active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn--new').addEventListener('click', init);
